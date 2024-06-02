1b625b8b4e8a58655e9bfbf02fd0421205002f20
uoya <uoya.dev@gmail.com>
2024-06-02 22:12:26 +0900

    Tweaked setup script for personal preferences
    Excluded non-essential configuration items for enabling extended session.
    
    Original setup script by [https://github.com/hu-ximing/Hyper-V-RHEL-Fedora-enhanced-session]

--- install-config.sh
+++ install-config.sh
@@ -34,16 +34,6 @@ systemctl enable xrdp-sesman
 sed -i_orig -e 's/port=3389/port=vsock:\/\/-1:3389/g' /etc/xrdp/xrdp.ini
 # use rdp security.
 sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
-# remove encryption validation.
-sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
-# disable bitmap compression since its local its much faster
-sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
-
-# rename the redirected drives to 'shared-drives'
-sed -i_orig -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
-
-# Change the allowed_users
-echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 
 
 #Ensure hv_sock gets loaded
